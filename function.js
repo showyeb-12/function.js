36292353b384401ac6dd529a8986c3296ff2ba4c
showyeb-12 <mdshowyebislam@gmail.com>
2026-07-25 00:09:46 +0600

    function.js

---
+++ function.js
@@ -0,0 +1,19 @@
+/**
+ * function
+ * give a name
+ * give too bracket
+ * after first bracket two second bracket
+ * inside the second bracket you will do the work for this function
+ *
+ */
+
+// function declaration
+function fanOnkor() {
+  console.log("step-1: walk towards the switch");
+  console.log("step-2: press the switch");
+  console.log("step-3: come back and do whatever you were doing");
+}
+
+function setTheMosquitoNet() {
+  console.log("mosquito no dinner for you!!!!");
+}
---
+++ revision.js
@@ -0,0 +1,17 @@
+/**
+ * variable
+ * conditionals
+ * loop
+ * array
+ * objects
+ *
+ */
+
+const price = 25;
+if (price > 20) {
+  console.log("i will buy it")      
+}
+for (let i = 0; i < 10; i++) {
+
+}
+const numbers = [2, 3, 4, 5, 6];
